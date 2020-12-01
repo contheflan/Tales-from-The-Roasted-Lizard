e05ed9d6c68d33558aabeb2e6babb35d306b6f27
Connor McKenna <connormckenna@Connors-MacBook-Pro.local>
2020-11-30 20:12:53 -0500

    updated user_require

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -46,6 +46,6 @@ class CommentsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def comment_params
-      params.require(:comment).permit(:name, :age)
+      params.require(:comment).permit(:name, :email, :password)
     end
 end
